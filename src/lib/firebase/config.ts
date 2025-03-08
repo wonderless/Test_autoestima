154ed93b7d4f4bc8a13c00c88d352428da7eb6d3
wonderless <csosalupuche@gmail.com>
2025-03-07 22:43:09 -0500

    vredadssssss

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -13,7 +13,7 @@ const firebaseConfig = {
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
   };
 
-//ss
+//ssss
 const app = initializeApp(firebaseConfig); 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
